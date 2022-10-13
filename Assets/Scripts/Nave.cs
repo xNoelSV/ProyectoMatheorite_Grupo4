6217acb6bcd471f3e7aa8a79ff40959c97dae0eb
Noel Sariñena <noelsarinena@ceroca.cat>
2022-10-13 17:15:53 +0200

    BugFix_1.0.0

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Nave : MonoBehaviour
 {
-    public float _vel = 10;
+    public float _vel = 8;
 
     public GameObject _prefabProjectil;
     public GameObject _posicioCanon;
@@ -18,6 +18,36 @@ public class Nave : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject projectil = Instantiate(_prefabProjectil);
+            projectil.transform.position = _posicioCanon.transform.position;
+        }
+        Vector2 minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(x: 0, y: 0));
+        Vector2 maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(x: 1, y: 1));
+
+        minPantalla.x += 0.6f;
+        maxPantalla.x -= 0.6f;
+
+        minPantalla.y += 0.6f;
+        maxPantalla.y -= 0.6f;
+
+        float direccioX = Input.GetAxisRaw("Horizontal");
+        float direccioy = Input.GetAxisRaw("Vertical");
+
+        //Debug.Log(message: "direccioX: " + direccioX + " - direccioY: " + direccioy);
+
+        Vector2 direccioIndicada = new Vector2(direccioX, direccioy).normalized;
+
+        //trobem pos actual nau:
+        Vector2 posNova = transform.position;
+        posNova += direccioIndicada * _vel * Time.deltaTime;
+
+        posNova.x = Mathf.Clamp(posNova.x, minPantalla.x, maxPantalla.x);
+        posNova.y = Mathf.Clamp(posNova.y, minPantalla.y, maxPantalla.y);
+
+        transform.position = posNova;
+
+        // Debug.Log(message: "posNova: " + posNova);
     }
 }
