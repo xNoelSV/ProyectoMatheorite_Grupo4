dc7eb73357df142b18acdf1f9ce043904de9a5b4
Noel Sariñena <noelsarinena@ceroca.cat>
2022-10-13 15:51:11 +0200

    Commit inicial

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour
+{
+    public float _vel = 10;
+
+    public GameObject _prefabProjectil;
+    public GameObject _posicioCanon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
