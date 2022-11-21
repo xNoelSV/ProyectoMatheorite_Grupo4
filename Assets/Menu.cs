11c70c14c6e9b0281a5b4e9f14ce7c300468b6e8
Jose Andres Chero Ascurra <jose_andreschero@ceroca.cat>
2022-11-21 17:41:49 +0100

    vFinalDeDia6.1.1
    Funcionalidades boton menu

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,7 @@ public class MainMenu : MonoBehaviour
         
     }
 
-    public void EscenaJuego() 
-    {
+    public void StartGame(){
         SceneManager.LoadScene("Jogo");
     }
 }
